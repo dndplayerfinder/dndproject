d1673b7d1940e4bcc28814ce75cf46a2fd0a206c
Killet <a348680@uabc.edu.mx>
2020-11-11 18:11:23 -0800

    Login y Registro
    Plantillas para hacer login y registro en una sola view cambiando de pestaña

--- player_finder/src/routes/authentication.js
+++ player_finder/src/routes/authentication.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/registro',(req,res)=>{
+    res.render('cuenta/registro');
+});
+router.post('/registro',(req,res)=>{
+});
 module.exports = router;
