8979c741ec800dde47a04033c222c9be8356da74
Killet <a348680@uabc.edu.mx>
2020-11-14 13:51:32 -0800

    Metodo de puntuacion
    Metodo para puntuar a los jugadores de acuerdo a los criterios de evaluacion de las historias

--- player_finder/src/routes/usuarios.js
+++ player_finder/src/routes/usuarios.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../database');
+const { post } = require('./authentication');
+const app = express();
 
-const pool=require|('../database');
+router.post('/rate_player',async(req,res)=>{
+    const {player_rated,puntuacion}= req.body
+    const rating ={
+        player_rated,
+        puntuacion
+    };
 
-router.get('/add',(req,res)=>{
-    res.render('usuarios/add');
+    try {
+        logged = await pool.query("insert into usuario_puntuacion(usuario_puntuador,usuario_punteado,puntaje)values(?,?,?) where not(usuario_punteador=? and usuario_punteado=?",[sess.login,rating.player_rated,rating.puntuacion,sess.login,rating.player_rated]);
+        res.send('Usuario Puntuado');
+    } catch (error) {
+        
+    }
 });
 
 module.exports = router;
